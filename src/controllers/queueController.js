a35dff0e91d9775161cd3bd026fbe1ee70e3ae3d
Edyr Costa <edyrcosta12@gmail.com>
2019-06-18 19:00:14 -0300

    Arquivo de controle de requisições das filas.

--- src/controllers/queueController.js
+++ src/controllers/queueController.js
@@ -4,7 +4,20 @@ exports.getQueues = (req, res, next) => {
     res.status(200).send(queueService.getQueues());
 };
 
-exports.insertQueues = (req, res) => {
-    queueService.insertQueues(req, res);
-    res.render("C:\\Users\\edyrc\\pro\\fcamaragerenciadorfila\\client\\index.ejs")
+exports.insertQueue = (req, res) => {
+	queueService.insertQueue(req, res);
+	res.status(200);
+	res.end();
+}
+
+exports.updateQueue = (req, res) => {
+	queueService.updateQueue(req, res);
+	res.status(200);
+	res.end();
+}
+
+exports.deleteQueue = (req, res) => {
+	queueService.deleteQueue(req, res);
+	res.status(200);
+	res.end();
 }
