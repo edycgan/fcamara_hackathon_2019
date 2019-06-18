a5fa98cc9780ca1487f24fd0c3a847303d5cb23f
Edyr Costa <edyrcosta12@gmail.com>
2019-06-18 19:00:45 -0300

    Arquivo de controle de requisições dos usuários.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,23 @@
 const userService = require('../services/userService');
 
-exports.getName = (req, res, next) => {
+exports.listUser = (req, res, next) => {
     res.status(200).send(userService.getName());
 };
+
+exports.insertUser = (req, res) => {
+    userService.insertUser(req, res);
+    res.status(200);
+    res.end();
+}
+
+exports.updateUser = (req, res) => {
+    userService.updateUser(req, res);
+    res.status(200);
+    res.end();
+}
+
+exports.deleteUser = (req, res) => {
+    userService.deleteUser(req, res);
+    res.status(200);
+    res.end();
+}
