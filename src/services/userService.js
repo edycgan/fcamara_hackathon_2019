bbe5e9c559fc70af76ccce3bda1ab4443c173ecd
Edyr Costa <edyrcosta12@gmail.com>
2019-06-18 18:58:52 -0300

    Código que fornece serviços de acesso aos dados dos usuários.

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,27 @@
-﻿const getName = () => {
-  return {
-    'name': 'Sophia'
-  }
+﻿const persistence = require('../dao/userPersistence');
+const user = require('../bean/user');
+const queue = require('../bean/queue');
+
+const insertUser = (req, res) => {
+    let objUser = new user(Number(req.body.id), req.body.nome, req.body.status);
+    let objQueue = new queue(Number(req.body.id_queue), "", []);
+    persistence.insertUser(objUser, objQueue);
+}
+
+const updateUser = (req, res) => {
+  let objUser = new user(Number(req.body.id), req.body.nome, req.body.status);
+  let objQueue = new queue(Number(req.body.id_queue), "", []);
+  persistence.updateUser(objUser, objQueue);
+}
+
+const deleteUser = (req, res) => {
+  let objUser = new user(Number(req.body.id), req.body.nome, req.body.status);
+  let objQueue = new queue(Number(req.body.id_queue), "", []);
+  persistence.deleteUser(objUser, objQueue);
 }
 
 module.exports = {
-  getName
+    insertUser,
+    updateUser,
+    deleteUser
 }
