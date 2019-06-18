481986060d9496a6dd97426f06a810b22d1dcb61
Edyr Costa <edyrcosta12@gmail.com>
2019-06-18 19:02:52 -0300

    Arquivo de rotas do usuário.

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,6 +3,9 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get('/name', userController.getName);
+router.get('/list', userController.listUser);
+router.post('/insert', userController.insertUser);
+router.post('/update', userController.updateUser);
+router.post('/delete', userController.deleteUser);
 
 module.exports = router;
