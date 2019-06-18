38172ba9d4f0f9ccf576919c86e4dbb6c7ef7ca4
Edyr Costa <edyrcosta12@gmail.com>
2019-06-18 19:02:26 -0300

    Arquivo de rotas da fila.

--- src/routes/queueRoute.js
+++ src/routes/queueRoute.js
@@ -5,6 +5,8 @@ const queueController = require('../controllers/queueController');
 const router = express.Router();
 
 router.get('/list', queueController.getQueues);
-router.post('/insert', queueController.insertQueues);
+router.post('/insert', queueController.insertQueue);
+router.post('/update', queueController.updateQueue);
+router.post('/delete', queueController.deleteQueue);
 
 module.exports = router;
