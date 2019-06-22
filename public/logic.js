67f2f46d5f3cbc9f244b9b3869ac3370bd1ffb55
Edyr Costa <edyrcosta12@gmail.com>
2019-06-22 09:23:40 -0300

    Adicionado as funções de editar fila, excluir fila, editar usuário, excluir usuário e verificação do tipo de status do usuário.

--- public/logic.js
+++ public/logic.js
@@ -1,8 +1,62 @@
+function btnQueueUpdateOnClick(paramButtonId) {
+	let i = paramButtonId.lastIndexOf("_");
+	let queueId = paramButtonId.substring(i + 1, paramButtonId.length);
+
+	$("#inp_queue_update_id").val(queueId);
+}
+
+function btnQueueDeleteOnClick(paramButtonId) {
+	let i = paramButtonId.lastIndexOf("_");
+	let queueId = paramButtonId.substring(i + 1, paramButtonId.length);
+
+	let formData = {
+		id: queueId
+	}
+
+	$.ajax({
+		type: "POST",
+		url: "/queue/delete",
+		data: formData
+	});
+
+	updateView();
+}
+
 function btnUserInsertOnClick(paramButtonId) {
 	let i = paramButtonId.lastIndexOf("_");
 	let queueId = paramButtonId.substring(i + 1, paramButtonId.length);
 	
 	$("#inp_queue_id").val(queueId);
+}
+
+function btnUserDeleteOnClick(paramButtonId) {
+	let i = paramButtonId.indexOf("*");
+	let j = paramButtonId.lastIndexOf("*");
+	let queueId = paramButtonId.substring(i + 1, j);
+	let userId = paramButtonId.substring(j + 1, paramButtonId.length);
+
+	let formData = {
+		id: userId,
+		id_queue: queueId
+	}
+
+	$.ajax({
+		type: "POST",
+		url: "/user/delete",
+		data: formData
+	});
+
+	updateView();
+}
+
+function btnUserUpdateOnClick(paramButtonId) {
+	let i = paramButtonId.indexOf("*");
+	let j = paramButtonId.lastIndexOf("*");
+	let queueId = paramButtonId.substring(i + 1, j);
+	let userId = paramButtonId.substring(j + 1, paramButtonId.length);
+
+	$("#inp_user_update_queue_id").val(queueId);
+	$("#inp_user_update_user_id").val(userId);
 }
 
 function formQueueSubmit(event) {
@@ -36,16 +90,60 @@ function formUserSubmit(event) {
 		type: "POST",
 		url: "/user/insert",
 		data: formData
-	})
+	});
 
 	$("#inp_user_name").val("");
 	$("#modalEntrarFila").modal("hide");
 	updateView();
 }
 
+function formUserUpdateSubmit(event) {
+	event.preventDefault();
+
+	let formData = {
+		id: $("#inp_user_update_user_id").val(),
+		nome: $("#inp_user_update_name").val(),
+		status: 2,
+		id_queue: $("#inp_user_update_queue_id").val()
+	}
+
+	$.ajax({
+		type: "POST",
+		url: "/user/update",
+		data: formData
+	});
+
+	$("inp_user_update_name").val("");
+	$("#mdlEditarUsuario").modal("hide");
+	
+	updateView();
+}
+
+function formQueueUpdateSubmit(event) {
+	event.preventDefault();
+
+	let formData = {
+		id: $("#inp_queue_update_id").val(),
+		nome: $("#inp_queue_update_name").val(),
+	}
+
+	$.ajax({
+		type: "POST",
+		url: "/queue/update",
+		data: formData
+	});
+
+	$("inp_queue_update_name").val("");
+	$("#mdlQueueUpdate").modal("hide");
+	
+	updateView();
+}
+
 $(document).ready(function () {
 	$("#form_queue").submit(formQueueSubmit);
 	$("#form_user").submit(formUserSubmit);
+	$("#form_user_update").submit(formUserUpdateSubmit);
+	$("#form_queue_update").submit(formQueueUpdateSubmit);
 
 	updateView();
 })
@@ -71,15 +169,33 @@ function updateViewSucess(data) {
 		btnQueueName.innerHTML = json[i].nome;
 		let btnUserInsert = divQueue.querySelector("#btn_user_insert_");
 		btnUserInsert.id += json[i].id;
+		let btnQueueUpdate = divQueue.querySelector("#btn_queue_update_");
+		btnQueueUpdate.id += json[i].id;
+		let btnQueueDelete = divQueue.querySelector("#btn_queue_delete_");
+		btnQueueDelete.id += json[i].id;
 
 		let tbdUsers = divQueue.querySelector("#tbd_users");
 		for (let j = 0; j < json[i].users.length; j++) {
 			let id = document.createTextNode(json[i].users[j].id);
 			let nome = document.createTextNode(json[i].users[j].nome);
-			let status = document.createTextNode(json[i].users[j].status);
+			let status = document.createTextNode(returnStatusName(json[i].users[j].status));
+
+			let buttonEdit = document.createElement("BUTTON");
+			buttonEdit.id = "btn_edit_user*" + json[i].id + "*" + json[i].users[j].id;
+			buttonEdit.innerHTML = "Editar";
+			buttonEdit.className = "btn btn-primary";
+			buttonEdit.setAttribute("data-toggle", "modal");
+			buttonEdit.setAttribute("data-target", "#mdlEditarUsuario");
+			buttonEdit.onclick = function() {
+				btnUserUpdateOnClick(this.id);
+			}
 			let buttonDelete = document.createElement("BUTTON");
+			buttonDelete.id = "btn_delete_user*" + json[i].id + "*" + json[i].users[j].id;
 			buttonDelete.innerHTML = "Deletar";
-			buttonDelete.className = "btn btn-danger";
+			buttonDelete.className = "btn btn-danger ml-3";
+			buttonDelete.onclick = function() {
+				btnUserDeleteOnClick(this.id);
+			}
 			
 			let row = tbdUsers.insertRow();
 			let cellId = row.insertCell();
@@ -89,9 +205,19 @@ function updateViewSucess(data) {
 			let cellStatus = row.insertCell();
 			cellStatus.appendChild(status);
 			let cellAction = row.insertCell();
+			cellAction.appendChild(buttonEdit);
 			cellAction.appendChild(buttonDelete);
 		}
 
 		divQueues.appendChild(divQueue);
+	}
+}
+
+function returnStatusName(statusId) {
+	switch(Number(statusId)) {
+		case 1:
+			return "Em atendimento"
+		case 2:
+			return "Aguardando";
 	}
 }
