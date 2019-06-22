13a0af2be70c3e5ba7fe3cdf20b3a708eeaf3fe2
Edyr Costa <edyrcosta12@gmail.com>
2019-06-22 09:40:00 -0300

    Alteração para permitir que a aplicação rode no heroku.

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,7 @@
 const app = require('../src/app');
 const config = require('../configs/config.json');
 
-const port = config.express.port;
+const port = process.env.PORT || config.express.port;
 const host = config.express.host;
 
 app.listen(port,host, function () {
